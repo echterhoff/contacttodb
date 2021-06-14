2bb8cabdded458f931cfed9c94d070222275bf71
Lars Echterhoff <lars.echterhoff@gmail.com>
2021-06-14 18:32:44 +0200

    Better stuff

--- contacttodb.php
+++ contacttodb.php
@@ -80,6 +80,7 @@ class plgContactContacttodb extends JPlugin //extends CMSPlugin implements Subsc
     protected $app;
 
     /**
+     * 
      * Plugin constructor
      *
      * @param object $subject
@@ -90,12 +91,15 @@ class plgContactContacttodb extends JPlugin //extends CMSPlugin implements Subsc
         parent::__construct($subject, $params);
         $this->_plugin = JPluginHelper::getPlugin('system', 'contacttodb');
 
-        $params = $this->params;
+        //$this->params = $params;
     }
+    // Execute after base contact validation has passed. Validate e.g. extra data given before proceeding to send/store the form
+    // function onValidateContact(&$contact, &$data){ }
 
+    // Executes after all validation plugin/processes have successfully validated the contact and data but before the mail has been sent
     function onSubmitContact(&$contact, &$data)
     {
-        if ($contact->id != 16)
+        if ($this->params->get('autoanswercontactid', -1) != $contact->id)
             return;
 
         $db = $this->db;
@@ -127,7 +131,81 @@ class plgContactContacttodb extends JPlugin //extends CMSPlugin implements Subsc
 
         $db->setQuery($query);
         $db->execute();
+
+        if ($this->params->get('autoanswerenabled', false)) {
+            $this->_sendEmail($data, $contact);
+        }
     }
 
-    // function onValidateContact(&$contact, &$data){ }
+    private function _sendEmail($data, $contact)
+    {
+        $app = JFactory::getApplication();
+
+        $contact->email_to = $data['contact_email'];
+
+        $mailfrom = $app->get('mailfrom');
+        $fromname = $app->get('fromname');
+        $sitename = $app->get('sitename');
+
+        $name    = $data['contact_name'];
+        $email   = JStringPunycode::emailToPunycode($data['contact_email']);
+        // $subject = $data['contact_subject'];
+        // $body    = $data['contact_message'];
+
+        // Prepare email body
+        // $prefix = JText::sprintf('COM_CONTACT_ENQUIRY_TEXT', JUri::base());
+        $autoanswerbody = $this->params->get('autoanswerbody','');
+        $autoanswerbody = str_replace('%name%', $name, $autoanswerbody);
+        $autoanswerbody = str_replace('%email%', $email, $autoanswerbody);
+        $autoanswerbody = str_replace('</p>', "\r\n", $autoanswerbody);
+        $autoanswerbody = str_replace('</br>', "\r\n", $autoanswerbody);
+        // $autoanswerbody = str_replace('<p>', "", $autoanswerbody);
+        $autoanswerbody = strip_tags($autoanswerbody);
+        $body   =  $autoanswerbody; //stripslashes($body);
+
+        // Load the custom fields
+        if (!empty($data['com_fields']) && $fields = FieldsHelper::getFields('com_contact.mail', $contact, true, $data['com_fields'])) {
+            $output = FieldsHelper::render(
+                'com_contact.mail',
+                'fields.render',
+                array(
+                    'context' => 'com_contact.mail',
+                    'item'    => $contact,
+                    'fields'  => $fields,
+                )
+            );
+
+            if ($output) {
+                $body .= "\r\n\r\n" . $output;
+            }
+        }
+
+        $mail = JFactory::getMailer();
+        $mail->addRecipient($contact->email_to);
+        $mail->addReplyTo($email, $name);
+        $mail->setSender(array($mailfrom, $fromname));
+        $mail->setSubject($sitename . ': ' . $this->params->get('autoanswersubject',''));
+        $mail->setBody($body);
+        $sent = $mail->Send();
+
+        // If we are supposed to copy the sender, do so.
+
+        // Check whether email copy function activated
+        // if ($emailCopyToSender == true && !empty($data['contact_email_copy']))
+        // {
+        // 	$copytext    = JText::sprintf('COM_CONTACT_COPYTEXT_OF', $contact->name, $sitename);
+        // 	$copytext    .= "\r\n\r\n" . $body;
+        // 	$copysubject = JText::sprintf('COM_CONTACT_COPYSUBJECT_OF', $subject);
+
+        // 	$mail = JFactory::getMailer();
+        // 	$mail->addRecipient($email);
+        // 	$mail->addReplyTo($email, $name);
+        // 	$mail->setSender(array($mailfrom, $fromname));
+        // 	$mail->setSubject($copysubject);
+        // 	$mail->setBody($copytext);
+        // 	$sent = $mail->Send();
+        // }
+
+        return $sent;
+    }
 }
